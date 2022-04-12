04e6dc510c921538a6e06f3df178747d0063423d
JanHoly <53258234+hongolem@users.noreply.github.com>
2022-04-12 22:26:35 +0200

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -11,25 +11,41 @@ led.plot(A1[0], A1[1])
 def on_button_pressed_a():
     global A1, A2
     if (A1[0] == 0 or A1[0] == 1) and (A1[1] == 0 or A1[1] == 1):
-        A2 = [A1[0], 4 - A1[1]]
-        led.unplot(A1[0], A1[1])
-        A1 = A2
-        led.plot(A2[0], A2[1])
+        posun_v1()
     elif (A1[0] == 0 or A1[0] == 1) and (A1[1] == 3 or A1[1] == 4):
-        A2 = [4 - A1[0], 0 - A1[1]]
-        led.unplot(A1[0], A1[1])
-        A1 = A2
-        led.plot(A2[0], A2[1])
+        posun_v2()
+    elif (A1[0] == 3 or A1[0] == 4) and (A1[1] == 3 or A1[1] == 4):
+        posun_v1()
+    elif (A1[0] == 3 or A1[0] == 4) and (A1[1] == 0 or A1[1] == 1):
+        posun_v2()
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_b():
     global A1, A2
-    A2 = [4 - A1[0], 0 - A1[1]]
-    led.unplot(A1[0], A1[1])
-    A1 = A2
-    led.plot(A2[0], A2[1])
+    if (A1[0] == 0 or A1[0] == 1) and (A1[1] == 0 or A1[1] == 1):
+        posun_v2()
+    elif (A1[0] == 0 or A1[0] == 1) and (A1[1] == 3 or A1[1] == 4):
+        posun_v1()
+    elif (A1[0] == 3 or A1[0] == 4) and (A1[1] == 3 or A1[1] == 4):
+        posun_v2()
+    elif (A1[0] == 3 or A1[0] == 4) and (A1[1] == 0 or A1[1] == 1):
+        posun_v1()
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 def stred():
     led.plot_brightness(2, 2, 127)
 basic.forever(stred)
+
+def posun_v1():
+    global A1, A2
+    A2 = [A1[0], 4 - A1[1]]
+    led.unplot(A1[0], A1[1])
+    A1 = A2
+    led.plot(A2[0], A2[1])
+
+def posun_v2():
+    global A1, A2
+    A2 = [4 - A1[0], A1[1]]
+    led.unplot(A1[0], A1[1])
+    A1 = A2
+    led.plot(A2[0], A2[1])
--- main.ts
+++ main.ts
@@ -8,25 +8,45 @@ led.plot(A1[0], A1[1])
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
     
     if ((A1[0] == 0 || A1[0] == 1) && (A1[1] == 0 || A1[1] == 1)) {
-        A2 = [A1[0], 4 - A1[1]]
-        led.unplot(A1[0], A1[1])
-        A1 = A2
-        led.plot(A2[0], A2[1])
+        posun_v1()
     } else if ((A1[0] == 0 || A1[0] == 1) && (A1[1] == 3 || A1[1] == 4)) {
-        A2 = [4 - A1[0], 0 - A1[1]]
-        led.unplot(A1[0], A1[1])
-        A1 = A2
-        led.plot(A2[0], A2[1])
+        posun_v2()
+    } else if ((A1[0] == 3 || A1[0] == 4) && (A1[1] == 3 || A1[1] == 4)) {
+        posun_v1()
+    } else if ((A1[0] == 3 || A1[0] == 4) && (A1[1] == 0 || A1[1] == 1)) {
+        posun_v2()
     }
     
 })
 input.onButtonPressed(Button.B, function on_button_pressed_b() {
     
-    A2 = [4 - A1[0], 0 - A1[1]]
-    led.unplot(A1[0], A1[1])
-    A1 = A2
-    led.plot(A2[0], A2[1])
+    if ((A1[0] == 0 || A1[0] == 1) && (A1[1] == 0 || A1[1] == 1)) {
+        posun_v2()
+    } else if ((A1[0] == 0 || A1[0] == 1) && (A1[1] == 3 || A1[1] == 4)) {
+        posun_v1()
+    } else if ((A1[0] == 3 || A1[0] == 4) && (A1[1] == 3 || A1[1] == 4)) {
+        posun_v2()
+    } else if ((A1[0] == 3 || A1[0] == 4) && (A1[1] == 0 || A1[1] == 1)) {
+        posun_v1()
+    }
+    
 })
 basic.forever(function stred() {
     led.plotBrightness(2, 2, 127)
 })
+function posun_v1() {
+    
+    A2 = [A1[0], 4 - A1[1]]
+    led.unplot(A1[0], A1[1])
+    A1 = A2
+    led.plot(A2[0], A2[1])
+}
+
+function posun_v2() {
+    
+    A2 = [4 - A1[0], A1[1]]
+    led.unplot(A1[0], A1[1])
+    A1 = A2
+    led.plot(A2[0], A2[1])
+}
+
